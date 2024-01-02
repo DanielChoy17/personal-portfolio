454cae1de5f82d75af7860de366dc0a424ca06df
Daniel Choy <danielcchoy17@gmail.com>
2024-01-01 22:51:51 -0800

    Finished adding contact information at the bottom of each screen

--- src/components/socials/Socials.js
+++ src/components/socials/Socials.js
@@ -8,18 +8,25 @@ export const Socials = () => {
     const { isDarkMode } = useDarkMode();
 
     return (
-        <div className="social-icons">
-            <a href="https://github.com/DanielChoy17" className={isDarkMode ? 'dark-github' : 'github'}>
-                <FontAwesomeIcon icon={faGithub} />
-            </a>
+        <div className="social-container">
+            <div className="social-icons">
+                <a href="https://github.com/DanielChoy17" className={isDarkMode ? 'dark-github' : 'github'}>
+                    <FontAwesomeIcon icon={faGithub} />
+                </a>
 
-            <a href="https://linkedin.com/in/daniel-choy-2705501b5" className="linkedin">
-                <FontAwesomeIcon icon={faLinkedin} />
-            </a>
+                <a href="https://linkedin.com/in/daniel-choy-2705501b5" className="linkedin">
+                    <FontAwesomeIcon icon={faLinkedin} />
+                </a>
 
-            <a href="mailto:danielcchoy17@gmail.com">
-                <img src={GoogleIcon} className="google"/>
-            </a>
+                <a href="mailto:danielcchoy17@gmail.com">
+                    <img src={GoogleIcon} className="google" alt="Email"/>
+                </a>
+            </div>
+
+            <div className="contact-info">
+                <p className={isDarkMode ? 'dark-text' : 'text'}>danielcchoy17@gmail.com</p>
+                <p className={isDarkMode ? 'dark-text' : 'text'}>(510) 857-6848</p>
+            </div>
         </div>
     );
 };
